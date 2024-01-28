8cfe36919302bab4d2acd27a7c98dad49a4eb765
UntoldSox <matteo@untoldgames.com>
2024-01-28 15:35:56 +0100

    milestone per davvero

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@onready var game_map: PackedScene = preload("res://maps/gameplay.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://maps/gameplay.tscn")
+	get_tree().change_scene_to_packed(game_map)
+	#get_tree().change_scene_to_file("res://maps/gameplay.tscn")
